a9c2cdbf82a391fe24d3e2aeebbd6b83e88e53a5
Fabrice <codinglone@gmail.com>
2023-04-29 12:10:09 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    /* This program generates a mad libbed story.
+    * Author: Laura
+    * Date:  2/19/2049*/
+    public static void main(String[] args) {
+        String name1 = "Codinglone";
+        String adjective1 = "Beautiful";
+        String adjective2 = "Ugly";
+        String adjective3 = "Rude";
+        String verb1 = "see";
+        String noun1 = "Cow";
+        String noun2 = "Teacher";
+        String noun3 = "Television";
+        String noun4 = "Car";
+        String noun5 = "Class";
+        String noun6 = "Laptop";
+        String name2 = "Chuzzi";
+        int number = 12;
+        String place1 = "Kigali";
+        String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+        System.out.println(story);
+    }
+}
